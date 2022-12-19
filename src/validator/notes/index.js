15d44a792e2e775055009549e96cb30fb8193de1
rozakarief <rozakarief05@gmail.com>
2022-12-19 14:58:48 +0700

    data validation implemented

---
+++ src/validator/notes/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable quotes */
+const InvariantError = require("../../exceptions/InvariantError");
+const { NotePayloadSchema } = require("./schema");
+
+const NotesValidator = {
+  validateNotePayload: (payload) => {
+    const validationResult = NotePayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+module.exports = NotesValidator;
